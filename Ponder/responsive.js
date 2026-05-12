c256ee2ab0abbe97b1ea6df42f24f054484cecc2
HawtonMAJ <hawtonmaj81421@gmail.com>
2026-05-12 10:01:35 -0600

    animation fix

--- Ponder/responsive.js
+++ Ponder/responsive.js
@@ -6,6 +6,7 @@ function handleMenuBtnClick(event) {
     // Grab nav tag and toggle hide class
     let nav = document.querySelector("nav");
     nav.classList.toggle("hide")
+    menuBtn.classList.toggle("change")
 }
 
 menuBtn.addEventListener("click", handleMenuBtnClick);
